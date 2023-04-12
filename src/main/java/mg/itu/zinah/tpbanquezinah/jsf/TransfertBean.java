01f86b1324db4efddd6d6a74c210de3d957b8fc4
zinahRajerison <zinahrajerison2000@gmail.com>
2023-04-12 21:11:04 +0200

    Traiter les erreurs et messages de succès

--- src/main/java/mg/itu/zinah/tpbanquezinah/jsf/TransfertBean.java
+++ src/main/java/mg/itu/zinah/tpbanquezinah/jsf/TransfertBean.java
@@ -8,6 +8,7 @@ import jakarta.ejb.EJB;
 import jakarta.enterprise.context.RequestScoped;
 import jakarta.inject.Named;
 import mg.itu.zinah.tpbanquezinah.ejb.GestionnaireCompte;
+import mg.itu.zinah.tpbanquezinah.entities.CompteBancaire;
 
 /**
  *
@@ -16,10 +17,11 @@ import mg.itu.zinah.tpbanquezinah.ejb.GestionnaireCompte;
 @Named(value = "transfert")
 @RequestScoped
 public class TransfertBean {
+
     private Long idSource;
     private Long idDestination;
     private int montant;
-    
+
     @EJB
     private GestionnaireCompte gc;
 
@@ -52,9 +54,37 @@ public class TransfertBean {
     public void setMontant(int montant) {
         this.montant = montant;
     }
-    public String transferer(){
-        gc.transferer(gc.findCompte(idSource), gc.findCompte(idDestination), montant);
+
+    public String transferer() {
+        boolean erreur = false;
+        CompteBancaire source = gc.findCompte(idSource);
+        CompteBancaire destination = gc.findCompte(idDestination);
+        if (source == null) {
+            // Message d'erreur associé au composant source ; form:source est l'id client
+            // si l'id du formulaire est "form" et l'id du champ de saisie de l'id de la source est "source"
+            // dans la page JSF qui lance le transfert.
+            Util.messageErreur("Aucun compte avec cet id !", "Aucun compte avec cet id !", "form:source");
+            erreur = true;
+        } else {
+            if (source.getSolde() < montant) { // à compléter pour le cas où le solde du compte source est insuffisant...
+                Util.messageErreur("Solde insuffisant!", "Solde insuffisant!", "form:montant");
+                erreur = true;
+            }
+        }
+        if (destination == null) {
+            // Message d'erreur associé au composant source ; form:source est l'id client
+            // si l'id du formulaire est "form" et l'id du champ de saisie de l'id de la source est "source"
+            // dans la page JSF qui lance le transfert.
+            Util.messageErreur("Aucun compte de destination avec cet id !", "Aucun compte de destination avec cet id !", "form:destination");
+            erreur = true;
+        }
+        if (erreur) { // en cas d'erreur, rester sur la même page
+            return null;
+        }
+        gc.transferer(source, destination, montant);
+// Message de succès ; addFlash à cause de la redirection.
+        Util.addFlashInfoMessage("Transfert correctement effectué");
         return "listeComptes";
     }
-    
+
 }
