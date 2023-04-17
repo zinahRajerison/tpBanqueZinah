cf43ea834ab6ac4cf00b12b1a349af135cc3d8f3
zinahRajerison <zinahrajerison2000@gmail.com>
2023-04-17 21:12:26 +0200

    Traitement de l'exception

--- src/main/java/mg/itu/zinah/tpbanquezinah/jsf/MouvementBean.java
+++ src/main/java/mg/itu/zinah/tpbanquezinah/jsf/MouvementBean.java
@@ -5,6 +5,7 @@
 package mg.itu.zinah.tpbanquezinah.jsf;
 
 import jakarta.ejb.EJB;
+import jakarta.ejb.EJBException;
 import jakarta.faces.application.FacesMessage;
 import jakarta.faces.component.UIComponent;
 import jakarta.faces.component.UIInput;
@@ -12,6 +13,7 @@ import jakarta.faces.context.FacesContext;
 import jakarta.faces.validator.ValidatorException;
 import jakarta.inject.Named;
 import jakarta.faces.view.ViewScoped;
+import jakarta.persistence.OptimisticLockException;
 import java.io.Serializable;
 import mg.itu.zinah.tpbanquezinah.ejb.GestionnaireCompte;
 import mg.itu.zinah.tpbanquezinah.entities.CompteBancaire;
@@ -24,77 +26,92 @@ import mg.itu.zinah.tpbanquezinah.jsf.util.Util;
 @ViewScoped
 public class MouvementBean implements Serializable {
 
-  @EJB
-  private GestionnaireCompte gestionnaireCompte;
-  
-  private Long id;
-  private CompteBancaire compte;
-  private String typeMouvement;
-  private int montant;
+    @EJB
+    private GestionnaireCompte gestionnaireCompte;
 
-  public int getMontant() {
-    return montant;
-  }
+    private Long id;
+    private CompteBancaire compte;
+    private String typeMouvement;
+    private int montant;
 
-  public void setMontant(int montant) {
-    this.montant = montant;
-  }
-
-  public String getTypeMouvement() {
-    return typeMouvement;
-  }
-
-  public void setTypeMouvement(String typeMouvement) {
-    this.typeMouvement = typeMouvement;
-  }
-
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public CompteBancaire getCompte() {
-    return compte;
-  }
-  
-  public void loadCompte() {
-    compte = gestionnaireCompte.findCompte(id);
-  }
-  
-  // La méthode doit avoir cette signature.
-  public void validateSolde(FacesContext fc, UIComponent composant, Object valeur) {
-    UIInput composantTypeMouvement = (UIInput)composant.findComponent("typeMouvement");
-    // Sans entrer dans les détails, il faut parfois utiliser
-    // getSubmittedValue() à la place de getLocalValue.
-    // typeMouvement n'est pas encore mis tant que la validation n'est pas finie.
-    String valeurTypeMouvement = (String)composantTypeMouvement.getLocalValue();
-    if (valeurTypeMouvement == null) {
-      // Pour le cas où l'utilisateur a soumis le formulaire sans indiquer le type du mouvement
-      return;
+    public int getMontant() {
+        return montant;
     }
-    if (valeurTypeMouvement.equals("retrait")) {
-      int retrait = (int) valeur;
-      if (compte.getSolde() < retrait) {
-        FacesMessage message
-                = new FacesMessage(FacesMessage.SEVERITY_ERROR,
-                                     "Le retrait doit être inférieur au solde du compte",
-                                     "Le retrait doit être inférieur au solde du compte");
-        throw new ValidatorException(message);
-      }
+
+    public void setMontant(int montant) {
+        this.montant = montant;
     }
-  }
-  
-  public String enregistrerMouvement() {
-    if (typeMouvement.equals("ajout")) {
-      gestionnaireCompte.deposer(compte, montant);
-    } else {
-      gestionnaireCompte.retirer(compte, montant);
+
+    public String getTypeMouvement() {
+        return typeMouvement;
     }
-    Util.addFlashInfoMessage("Mouvement enregistré sur compte de " + compte.getNom());
-    return "listeComptes?faces-redirect=true";
-  }
+
+    public void setTypeMouvement(String typeMouvement) {
+        this.typeMouvement = typeMouvement;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public CompteBancaire getCompte() {
+        return compte;
+    }
+
+    public void loadCompte() {
+        compte = gestionnaireCompte.findCompte(id);
+    }
+
+    // La méthode doit avoir cette signature.
+    public void validateSolde(FacesContext fc, UIComponent composant, Object valeur) {
+        UIInput composantTypeMouvement = (UIInput) composant.findComponent("typeMouvement");
+        // Sans entrer dans les détails, il faut parfois utiliser
+        // getSubmittedValue() à la place de getLocalValue.
+        // typeMouvement n'est pas encore mis tant que la validation n'est pas finie.
+        String valeurTypeMouvement = (String) composantTypeMouvement.getLocalValue();
+        if (valeurTypeMouvement == null) {
+            // Pour le cas où l'utilisateur a soumis le formulaire sans indiquer le type du mouvement
+            return;
+        }
+        if (valeurTypeMouvement.equals("retrait")) {
+            int retrait = (int) valeur;
+            if (compte.getSolde() < retrait) {
+                FacesMessage message
+                        = new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                                "Le retrait doit être inférieur au solde du compte",
+                                "Le retrait doit être inférieur au solde du compte");
+                throw new ValidatorException(message);
+            }
+        }
+    }
+
+    public String enregistrerMouvement() {
+        try {
+            if (typeMouvement.equals("ajout")) {
+                gestionnaireCompte.deposer(compte, montant);
+            } else {
+                gestionnaireCompte.retirer(compte, montant);
+            }
+            Util.addFlashInfoMessage("Mouvement enregistré sur compte de " + compte.getNom());
+            return "listeComptes?faces-redirect=true";
+        } catch (EJBException ex) {
+            Throwable cause = ex.getCause();
+            if (cause != null) {
+                if (cause instanceof OptimisticLockException) {
+                    Util.messageErreur("Le compte de " + compte.getNom()
+                            + " a été modifié ou supprimé par un autre utilisateur !");
+                } else { // Afficher le message de ex si la cause n'est pas une OptimisticLockException
+                    Util.messageErreur(cause.getMessage());
+                }
+            } else { // Pas de cause attachée à l'EJBException
+                Util.messageErreur(ex.getMessage());
+            }
+            return null; // pour rester sur la page s'il y a une exception
+        }
+
+    }
 }
-  
